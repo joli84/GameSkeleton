99957f180534e152eecbbdbbb24d2fa41cab504e
joli84 <johan.ltu@gmail.com>
2015-04-12 21:51:56 +0200

    package name change

---
+++ GameSkeleton/src/se/lindgrendevelopment/gameskeleton/utils/Commons.java
@@ -0,0 +1,11 @@
+package se.lindgrendevelopment.gameskeleton.utils;
+
+public class Commons 
+{
+	public static final String TITLE = "My title!";
+	public static final int WINDOW_WIDTH = 160;
+	public static final int WINDOW_HEIGHT = 120;
+	public static final int WINDOW_SCALE = 3;
+	
+	public static final int AMMOUNT_OF_TICKS = 60;
+}
